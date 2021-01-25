d1a9672a5d1c72372f445d79ea04db59ddc3474d
gabrielrds1 <gabrielrds.grds@gmail.com>
2021-01-25 10:15:27 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-%matplotlib inline
-
-df = pd.read_html("file:///C:/Users/gabriel/AppData/Local/Temp/Rar$EXa1040.11915/d_lotfac.htm")
-df
